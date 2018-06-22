646166799976ec93493dd03e49f49834efa77676
Tai788 <Phantomlancer453@gmail.com>
2018-06-22 09:44:16 +0700

    fix complete

--- AnimationOfWord/app/src/main/java/com/example/admin/animationofword/MainActivity.java
+++ AnimationOfWord/app/src/main/java/com/example/admin/animationofword/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.admin.animationofword;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -71,47 +73,7 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     //xu ly su kien khi view nam o tren chuyen xuong duoi
                     if (viewGroup == place) {
-                        final RelativeLayout locationView = findViewById((int) v.getTag());
-                        final RelativeLayout parentView = (RelativeLayout) v.getParent();
-//                         parentView.removeView(v);
-//                         updateView(view);
-                        int[] location = new int[2];
-                        locationView.getLocationInWindow(location);
-//                        layoutParams.removeRule(RelativeLayout.BELOW);
-//                        layoutParams.removeRule(RelativeLayout.RIGHT_OF);
-//                        layoutParams.removeRule(RelativeLayout.ALIGN_PARENT_LEFT);
-//                        locationView.addView(v, layoutParams);
-
-
-                        TranslateAnimation animation = new TranslateAnimation(v.getX(), location[0], v.getY(),
-                                location[1]);
-                        animation.setRepeatMode(0);
-                        animation.setDuration(100);
-                        animation.setFillAfter(true);
-                        animation.setAnimationListener(new Animation.AnimationListener() {
-                            @Override
-                            public void onAnimationStart(Animation animation) {
-                                Log.d("TAGGG", "onAnimationStart: ");
-                            }
-
-                            @Override
-                            public void onAnimationEnd(Animation animation) {
-                                parentView.removeView(v);
-                                updateRelativeViewWhenRemove(view);
-                                layoutParams.removeRule(RelativeLayout.BELOW);
-                                layoutParams.removeRule(RelativeLayout.RIGHT_OF);
-                                layoutParams.removeRule(RelativeLayout.ALIGN_PARENT_LEFT);
-                                locationView.addView(v, layoutParams);
-                            }
-
-                            @Override
-                            public void onAnimationRepeat(Animation animation) {
-
-                            }
-                        });
-                        v.requestLayout();
-                        v.startAnimation(animation);
-
+                        backToFirstPlace(v, layoutParams);
                     }
                 }
             }
@@ -119,12 +81,58 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void backToFirstPlace(final View v, final RelativeLayout.LayoutParams layoutParams) {
+        stopAnimation(v);
+        final RelativeLayout locationView = findViewById((int) v.getTag());
+        final RelativeLayout parentView = (RelativeLayout) v.getParent();
+        int[] location = new int[2];
+        locationView.getLocationInWindow(location);
+        //back from top to bottom set location 1, else - location 1
+        TranslateAnimation animationDown = new TranslateAnimation(0, location[0]-v.getX(), 0,
+                location[1] - v.getY());
+        animationDown.setRepeatMode(0);
+        animationDown.setDuration(100);
+        animationDown.setFillAfter(true);
+        animationDown.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                Log.d("TAGGG", "onAnimationStart: ");
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                swapViewWhenRemove(parentView, v, layoutParams, locationView);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        v.requestLayout();
+        v.startAnimation(animationDown);
+    }
+
+    private void swapViewWhenRemove(final RelativeLayout parentView, final View v, final RelativeLayout.LayoutParams layoutParams, final RelativeLayout locationView) {
+        boolean handler = new Handler().post(new Runnable() {
+            public void run() {
+                parentView.removeView(v);
+                updateRelativeViewWhenRemove(view);
+                layoutParams.removeRule(RelativeLayout.BELOW);
+                layoutParams.removeRule(RelativeLayout.RIGHT_OF);
+                layoutParams.removeRule(RelativeLayout.ALIGN_PARENT_LEFT);
+                locationView.addView(v, layoutParams);
+            }
+        });
+    }
+
     private void leftRelative(View v, RelativeLayout.LayoutParams layoutParams) {
         if (view.getChildCount() != 0) {
             layoutParams.addRule(RelativeLayout.RIGHT_OF, view.getChildAt(view.getChildCount() - 1).getId());
         }
         int[] location = new int[2];
-        v.getLocationInWindow(location);
+        RelativeLayout parentView = (RelativeLayout) v.getParent();
+        parentView.getLocationInWindow(location);
         int x = view.getWidth() - location[0];
         int y = -(location[1] - space);
         RelativeLayout relativeLayout = (RelativeLayout) v.getParent();
@@ -134,7 +142,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void downLineRelative(View v, RelativeLayout.LayoutParams layoutParams) {
         int[] location = new int[2];
-        v.getLocationInWindow(location);
+        RelativeLayout parentView = (RelativeLayout) v.getParent();
+        parentView.getLocationInWindow(location);
         int x = (int) view.getChildAt(0).getX() - location[0];
         int y = -(location[1] - space - view.getHeight());
         RelativeLayout relativeLayout = (RelativeLayout) v.getParent();
@@ -145,7 +154,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void downLineLeftRelative(View v, RelativeLayout.LayoutParams layoutParams) {
         int[] location = new int[2];
-        v.getLocationInWindow(location);
+        RelativeLayout parentView = (RelativeLayout) v.getParent();
+        parentView.getLocationInWindow(location);
         layoutParams.addRule(RelativeLayout.BELOW, view.getChildAt(0).getId());
         layoutParams.addRule(RelativeLayout.RIGHT_OF, view.getChildAt(view.getChildCount() - 1).getId());
         int x = (int) view.getChildAt(view.getChildCount() - 1).getX()
@@ -157,27 +167,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void translateUp(final RelativeLayout parentView, final View v, final RelativeLayout.LayoutParams layoutParams, int margin, int toX, int toY) {
-        TranslateAnimation animation = new TranslateAnimation(0, (toX + margin), 0,
+        TranslateAnimation animationUp = new TranslateAnimation(0, (toX + margin), 0,
                 (toY + margin));
-        animation.setRepeatMode(0);
-        animation.setDuration(100);
-        animation.setFillAfter(true);
-        animation.setAnimationListener(new Animation.AnimationListener() {
+        animationUp.setRepeatMode(0);
+        animationUp.setDuration(100);
+        animationUp.setFillAfter(true);
+        animationUp.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
-                RelativeLayout viewGroup = (RelativeLayout) v.getParent();
-                viewGroup.removeView(v);
+//                final RelativeLayout viewGroup = (RelativeLayout) v.getParent();
+//                new Handler().post(new Runnable() { public void run() {  viewGroup.removeView(v); } });
+//                viewGroup.removeView(v);
             }
 
             @SuppressLint("ResourceAsColor")
             @Override
             public void onAnimationEnd(Animation animation) {
+                RelativeLayout viewGroup = (RelativeLayout) v.getParent();
+                viewGroup.removeView(v);
                 if (parentView.getChildCount() != 0) {
                     parentView.addView(v, layoutParams);
                     v.setEnabled(true);
                 } else {
                     parentView.addView(v);
                 }
+
             }
 
             @Override
@@ -186,7 +200,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         v.requestLayout();
-        v.startAnimation(animation);
+        v.startAnimation(animationUp);
     }
 
     private void updateRelativeViewWhenRemove(RelativeLayout view) {
@@ -224,6 +238,17 @@ public class MainActivity extends AppCompatActivity {
                 check = true;
             }
         }
+    }
+
+    public void stopAnimation(View v) {
+        v.clearAnimation();
+        if (canCancelAnimation()) {
+            v.animate().cancel();
+        }
+    }
+
+    public static boolean canCancelAnimation() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH;
     }
 }
 
